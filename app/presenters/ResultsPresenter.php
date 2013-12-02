ecf0bc4bca32961c80de016425244529e5938a45
St1c <cs@dataworkers.eu>
2013-12-02 19:36:11 +0100

    Added basic Results presenter

---
+++ app/presenters/ResultsPresenter.php
@@ -0,0 +1,32 @@
+<?php 
+
+namespace App;
+
+use Nette;
+
+class ResultsPresenter extends BaseSignedPresenter 
+{
+
+
+	/**
+	 * Add new result
+	 */
+	public function actionAdd($token)
+	{
+		
+		// Check if user is assigned to this task
+
+	}
+
+
+
+	/**
+	 * Add money to user wallet
+	 */
+	public function actionDefault($token)
+	{
+		// check ownership
+	}
+
+
+}
