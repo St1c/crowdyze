1c5a855a6c230a4de305db9ad0d1da87b54c8f1a
St1c <bruno.gardlo@gmail.com>
2014-04-08 22:26:48 +0200

    Fixing iOS modal window bug - window was not possible to close

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,63 @@
+(function($){
+
+$.fn.autoGrowInput = function(o) {
+
+	o = $.extend({
+		maxWidth: 1000,
+		minWidth: 0,
+		comfortZone: 70
+	}, o);
+
+	this.filter('input:text').each(function(){
+
+		var minWidth = o.minWidth || $(this).width(),
+			val = '',
+			input = $(this),
+			testSubject = $('<tester/>').css({
+				position: 'absolute',
+				top: -9999,
+				left: -9999,
+				width: 'auto',
+				fontSize: input.css('fontSize'),
+				fontFamily: input.css('fontFamily'),
+				fontWeight: input.css('fontWeight'),
+				letterSpacing: input.css('letterSpacing'),
+				whiteSpace: 'nowrap'
+			}),
+			check = function() {
+
+				if (val === (val = input.val())) {return;}
+
+				// Enter new content into testSubject
+				var escaped = val.replace(/&/g, '&amp;').replace(/\s/g,' ').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+				testSubject.html(escaped);
+
+				// Calculate new width + whether to change
+				var testerWidth = testSubject.width(),
+					newWidth = (testerWidth + o.comfortZone) >= minWidth ? testerWidth + o.comfortZone : minWidth,
+					currentWidth = input.width(),
+					isValidWidthChange = (newWidth < currentWidth && newWidth >= minWidth)
+										 || (newWidth > minWidth && newWidth < o.maxWidth);
+
+				// Animate width
+				if (isValidWidthChange) {
+					input.width(newWidth);
+				}
+
+			};
+
+		testSubject.insertAfter(input);
+
+		$(this).bind('keyup keydown blur update', check);
+
+	});
+
+	return this;
+
+};
+
+})(jQuery);
+
 $(function(){
 
 	/**
@@ -258,8 +318,8 @@ $(function(){
 			}
 
 			var self = this;
-			$(document).on('click', '#modal-window [data-dismiss="modal"]', function(e) {
-			///$('#modal-window [data-dismiss="modal"]').click(function(e) {
+			// $(document).on('click', '#modal-window [data-dismiss="modal"]', function(e) {
+			$('#modal-window [data-dismiss="modal"]').click(function(e) { // Fixes iOS closing bug
 				self.doCloseModal();
 				history.replaceState(null, null, self.originalUrl);
 				self.changeCount = 0;
@@ -412,23 +472,23 @@ $(function(){
 	},
 
 
-    $('#showLeft').on('click', function(e) {
-        $(this).toggleClass('active');
-        $('#cbp-spmenu-s1').toggleClass('cbp-spmenu-open');
-        $('#cbp-overlay').toggleClass('show');
-        if ($(this).hasClass('active')) {
-            $('body').css('overflow', 'hidden');
-        } else {
-            $('body').css('overflow', 'visible');
-        }
+	$('#showLeft').on('click', function(e) {
+		$(this).toggleClass('active');
+		$('#cbp-spmenu-s1').toggleClass('cbp-spmenu-open');
+		$('#cbp-overlay').toggleClass('show');
+		if ($(this).hasClass('active')) {
+			$('body').css('overflow', 'hidden');
+		} else {
+			$('body').css('overflow', 'visible');
+		}
 
-    });
+	});
 
 
-    $('.filter-close, .cbp-overlay').on('click', function(e) {
-        $('#showLeft').click();
-    });
-
+	$('.filter-close, .cbp-overlay').on('click', function(e) {
+		$('#showLeft').click();
+	});
+		
 	$(".loader").css('background-image', 'none').fadeOut(1000);
 
 });
